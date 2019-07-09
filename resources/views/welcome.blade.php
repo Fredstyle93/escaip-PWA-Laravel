a48e994920a3a5bb4164d056e31846e45e1be7d1
fabrice <fgarrec@hotmail.com>
2019-07-09 07:42:38 -0400

    SUBNAV added, NAVBAR fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,12 +25,30 @@
 <body>
     <section class="site-container">
         <!-- --------------------------------- NAV BAR --------------------------------- -->
-        <section class="navbar">
+        <section class="navbar-l">
             <section class="navbar-wrapper">
                 <nav class="navbar-links">
                     <a href="" class="navbar-link"><img src="images/icons/notifications.svg" alt=""></a>
                     <a href="" class="navbar-link"><img src="images/icons/add.svg" alt=""></a>
                     <a href="" class="navbar-link"><img src="images/users/profil.jpg" alt=""></a>
+                </nav> 
+            </section>
+        </section>
+
+        <!-- --------------------------------- CONTENT --------------------------------- -->
+
+        <section class="content">
+
+        </section>
+
+        <!-- --------------------------------- SUBNAV --------------------------------- -->
+
+        <section class="subnav-l">
+            <section class="subnav-wrapper">
+                <nav class="subnav-links">
+                    <a href="" class="subnav-link"><img src="images/icons/notifications.svg" alt=""></a>
+                    <a href="" class="subnav-link"><img src="images/icons/add.svg" alt=""></a>
+                    <a href="" class="subnav-link"><img src="images/users/profil.jpg" alt=""></a>
                 </nav> 
             </section>
         </section>
