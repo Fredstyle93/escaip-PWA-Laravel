87b4f4509973f627d0ae3b88c25744c2b2c5cdba
fabrice <fgarrec@hotmail.com>
2019-07-12 08:37:09 -0400

    GENERAL LAYOUT upgrade, RESPONSIVE upgrade, SIDEBAR fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,14 +29,36 @@
                 <!-- --------------------------------- SIDEBAR --------------------------------- -->
 
                 <aside class="sidebar-l">
-                    <section class="sidebar-wrapper">
-                        <nav class="sidebar-links">
-                            <a href="" class="sidebar-link {{\Request::is('home') ? 'is-active' : ''}}"><img src="images/icons/home.svg" alt=""></a>
-                            <a href="" class="sidebar-link {{\Request::is('projects') ? 'is-active' : ''}}"><img src="images/icons/portfolio-icon.svg" alt=""></a>
-                            <a href="" class="sidebar-link {{\Request::is('school') ? 'is-active' : ''}}"><img src="images/icons/school-icon.svg" alt=""></a>
-                            <a href="" class="sidebar-link {{\Request::is('calendar') ? 'is-active' : ''}}"><img src="images/icons/calendar-icon.svg" alt=""></a>
-                        </nav>
-                    </section>
+                    <nav class="sidebar-wrapper">
+                        <ul class="sidebar-links">
+                            <a href="" class="sidebar-link-container {{\Request::is('home') ? 'is-active' : ''}}">
+                                <li class="sidebar-link">
+                                    <img src="images/icons/home.svg" alt=""> <span class="sidebar-link-text"> {{__('Accueil')}} </span> 
+                                </li>
+                            </a>
+                            <a href="" class="sidebar-link-container {{\Request::is('projects') ? 'is-active' : ''}}">
+                                <li class="sidebar-link">
+                                    <img src="images/icons/portfolio-icon.svg" alt=""> <span class="sidebar-link-text"> {{__('Projets')}} </span> 
+                                </li>
+                            </a>
+                            <a href="" class="sidebar-link-container {{\Request::is('school') ? 'is-active' : ''}}">
+                                <li class="sidebar-link">
+                                    <img src="images/icons/school-icon.svg" alt=""> <span class="sidebar-link-text"> {{__('Cours')}} </span> 
+                                </li>
+                            </a>
+                            <a href="" class="sidebar-link-container {{\Request::is('calendar') ? 'is-active' : ''}}">
+                                <li class="sidebar-link">
+                                    <img src="images/icons/calendar-icon.svg" alt=""> <span class="sidebar-link-text"> {{__('Calendriers')}} </span> 
+                                </li>
+                            </a>
+
+                            {{-- <a href="index.html" class="sidebar-menu-link">
+                                <li class="sidebar-menu-items sidebar-menu-items-selected">
+                                    <span class="sprite sprite-sidebar-home"></span> <span class="sidebar-menu-items-name">Accueil</span>
+                                </li>
+                            </a> --}}
+                        </ul>
+                    </nav>
                 </aside>
         
                 <!-- --------------------------------- CONTENT --------------------------------- -->
